4d891e9018ed269fa28a1ee6fca6db057dce1960
Mr. D.A <sturgisMrDA@users.noreply.github.com>
2018-05-08 06:33:16 -0400

    "Build URL for API call."

--- app/src/main/java/com/example/android/popcornmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popcornmovies/MainActivity.java
@@ -13,8 +13,7 @@ import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
-    // Reviewer or GitHub cloner: Add your API key in strings.xml with name themoviedb_api_key.
-    private String themoviedbApiKey = getResources().getString(R.string.themoviedb_api_key);
+
     private TextView captionTextView, messageTextView;
 
     @Override
@@ -25,13 +24,12 @@ public class MainActivity extends AppCompatActivity {
         Picasso.with(this)
                 .load("http://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg")
                 .into(moviePosterImageView);
-        messageTextView = (TextView) findViewById(R.id.mesage_tv);
-        messageTextView.setText(themoviedbApiKey);
+        // Reviewer or GitHub cloner: Add your API key in strings.xml with name themoviedb_api_key.
+        String themoviedbApiKey = getString(R.string.themoviedb_api_key);
+
         captionTextView = (TextView) findViewById(R.id.caption_tv);
         String sortOrder = "popular";
-        URL tmdbUrl = NetworkUtils.buildUrl(this,sortOrder);
+        URL tmdbUrl = NetworkUtils.buildUrl(this, sortOrder, themoviedbApiKey);
         captionTextView.setText(tmdbUrl.toString());
-
-
     }
 }
