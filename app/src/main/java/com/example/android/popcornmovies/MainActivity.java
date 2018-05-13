a90228334497e26094c058ca753312cdb62c58cb
Aaron Dunigan AtLee <aaron.dunigan.atlee@gmail.com>
2018-05-13 19:20:53 -0400

    Add Recyclerview

--- app/src/main/java/com/example/android/popcornmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popcornmovies/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.popcornmovies.utilities.NetworkUtils;
-import com.squareup.picasso.Picasso;
+import com.example.android.popcornmovies.utilities.PosterGridAdapter;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,22 +20,28 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private TextView captionTextView, messageTextView;
+    private RecyclerView.LayoutManager posterGridLayoutManager;
+    private PosterGridAdapter gridAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView moviePosterImageView = (ImageView) findViewById(R.id.movie_poster_iv);
-        Picasso.with(this)
-                .load("http://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg")
-                .into(moviePosterImageView);
+        // Set up RecyclerView with a GridLayoutManager
+        RecyclerView posterGridRecyclerView = (RecyclerView) findViewById(R.id.poster_grid_rv);
+        posterGridLayoutManager = new GridLayoutManager(this, 3);
+        posterGridRecyclerView.setLayoutManager(posterGridLayoutManager);
+        // Set adapter for the RecyclerView.
+        gridAdapter = new PosterGridAdapter(this);
+        posterGridRecyclerView.setAdapter(gridAdapter);
+
         // Reviewer or GitHub cloner: Add your API key in strings.xml with name themoviedb_api_key.
         String themoviedbApiKey = getString(R.string.themoviedb_api_key);
 
-        captionTextView = (TextView) findViewById(R.id.caption_tv);
+        //captionTextView = (TextView) findViewById(R.id.caption_tv);
         String sortOrder = "popular";
         URL tmdbUrl = NetworkUtils.buildUrl(this, sortOrder, themoviedbApiKey);
-        captionTextView.setText(tmdbUrl.toString());
+        //captionTextView.setText(tmdbUrl.toString());
         MovieDbQueryTask fetchMoviesTask = new MovieDbQueryTask();
         fetchMoviesTask.execute(tmdbUrl);
     }
@@ -53,10 +62,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String queryResult) {
             super.onPostExecute(queryResult);
-            if (queryResult != null) {
+            /*if (queryResult != null) {
                 messageTextView = findViewById(R.id.mesage_tv);
                 messageTextView.setText(queryResult);
-            }
+            }*/
             return;
         }
     }
---
+++ app/src/main/java/com/example/android/popcornmovies/utilities/PosterGridAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.popcornmovies.utilities;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.popcornmovies.R;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by Dunigan AtLee on 5/11/2018.
+ */
+
+public class PosterGridAdapter extends RecyclerView.Adapter<PosterGridAdapter.PosterGridAdapterViewHolder> {
+    public Context adapterContext;
+    public PosterGridAdapter(Context context) {
+        adapterContext = context;
+    }
+    // Create a ViewHolder
+    public class PosterGridAdapterViewHolder extends RecyclerView.ViewHolder {
+        public final ImageView posterImageView;
+
+        public PosterGridAdapterViewHolder(View view) {
+            super(view);
+            posterImageView = (ImageView) view.findViewById(R.id.movie_poster_iv);
+        }
+    }
+
+    @NonNull
+    @Override
+    public PosterGridAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        int layoutIdForGridItem = R.layout.movie_poster_grid_item;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(layoutIdForGridItem, parent, false);
+        PosterGridAdapterViewHolder viewHolder = new PosterGridAdapterViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(PosterGridAdapterViewHolder holder, int position) {
+        // Load poster image into posterImageView which was initialized in the ViewHolder constructor.
+        Picasso.with(adapterContext)
+                .load("http://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg")
+                .into(holder.posterImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 20;
+    }
+
+}
