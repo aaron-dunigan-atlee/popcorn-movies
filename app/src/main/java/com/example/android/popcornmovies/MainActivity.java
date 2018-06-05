c56787f36e0d639609c4a6dfb6332a181fee4515
Aaron Dunigan AtLee <aaron.dunigan.atlee@gmail.com>
2018-06-04 22:25:16 -0400

    "Add settings activity."

--- app/src/main/java/com/example/android/popcornmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popcornmovies/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.popcornmovies;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.android.popcornmovies.utilities.NetworkUtils;
@@ -17,32 +22,52 @@ import java.net.URL;
 
 public class MainActivity
         extends AppCompatActivity
-        implements PosterGridAdapter.OnClickHandler {
+        implements PosterGridAdapter.OnClickHandler, SharedPreferences.OnSharedPreferenceChangeListener{
 
     private TextView captionTextView, messageTextView;
     private RecyclerView.LayoutManager posterGridLayoutManager;
     private PosterGridAdapter gridAdapter;
     private String mMovieQueryResult = null;
+    private String sortOrder, themoviedbApiKey;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.popcorn_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.open_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupPreferences();
         // Set up RecyclerView with a GridLayoutManager
         RecyclerView posterGridRecyclerView = (RecyclerView) findViewById(R.id.poster_grid_rv);
         posterGridLayoutManager = new GridLayoutManager(this, 3);
         posterGridRecyclerView.setLayoutManager(posterGridLayoutManager);
         // Set adapter for the RecyclerView.
-        gridAdapter = new PosterGridAdapter(this,this);
+        gridAdapter = new PosterGridAdapter(this, this);
         posterGridRecyclerView.setAdapter(gridAdapter);
-
         // Reviewer or GitHub cloner: Add your API key in strings.xml with name themoviedb_api_key.
-        String themoviedbApiKey = getString(R.string.themoviedb_api_key);
+        themoviedbApiKey = getString(R.string.themoviedb_api_key);
+        fetchMovies();
+    }
 
-        //captionTextView = (TextView) findViewById(R.id.caption_tv);
-        String sortOrder = "popular";
+    private void fetchMovies() {
         URL tmdbUrl = NetworkUtils.buildUrl(this, sortOrder, themoviedbApiKey);
-        //captionTextView.setText(tmdbUrl.toString());
         MovieDbQueryTask fetchMoviesTask = new MovieDbQueryTask();
         fetchMoviesTask.execute(tmdbUrl);
     }
@@ -50,6 +75,16 @@ public class MainActivity
     @Override
     public void onItemClicked(int position) {
         launchDetailActivity(position, mMovieQueryResult);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.order_preference_key))) {
+            setSortOrder(sharedPreferences.getString(
+                    getString(R.string.order_preference_key),
+                    getString(R.string.order_preference_default)));
+            fetchMovies();
+        }
     }
 
     public class MovieDbQueryTask extends AsyncTask<URL, Void, String> {
@@ -87,4 +122,31 @@ public class MainActivity
         // Launch activity
         startActivity(detailActivityIntent);
     }
+    // Set up all preferences by loading them from SharedPreferences,
+    // and register the SharedPreferencesChangeListener so that future preference updates
+    // are noticed.
+    // This method is called during onCreate().
+    private void setupPreferences(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        setSortOrder(sharedPreferences.getString(
+                getString(R.string.order_preference_key),
+                getString(R.string.order_preference_default)));
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroy();
+    }
+
+    public String getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(String sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
 }
---
+++ app/src/main/java/com/example/android/popcornmovies/SettingsFragment.java
@@ -0,0 +1,47 @@
+package com.example.android.popcornmovies;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preferences_popcorn);
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int preferenceCount = preferenceScreen.getPreferenceCount();
+        for (int i=0; i<preferenceCount; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+            if (preference instanceof ListPreference) {
+                preference.setSummary(((ListPreference) preference).getEntry());
+            }
+        }
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (preference != null) {
+            if (preference instanceof ListPreference) {
+                preference.setSummary(((ListPreference) preference).getEntry());
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
